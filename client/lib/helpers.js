0e3de0c0d627c2c06cde01eec5b9449bd0585fca
Yondon Fu <yondon.fu@gmail.com>
2018-04-26 16:19:38 -0400

    Quick fix to filter out empty strings from account set

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -5,7 +5,7 @@ const { toBuffer, addHexPrefix } = require("ethereumjs-util")
 
 const makeTree = async accountsFile => {
     const data = await promisify(fs.readFile)(accountsFile)
-    const accounts = data.toString().split("\n")
+    const accounts = data.toString().split("\n").filter(acct => acct)
     const sortedAccounts = [...new Set(accounts)].map(acct => toBuffer(addHexPrefix(acct))).sort(Buffer.compare)
 
     return new MerkleTree(sortedAccounts)
